2550a7b736562d3661864939aeee947add759d93
Luciano Werle <110618631+LucianoWerle@users.noreply.github.com>
2023-09-24 13:23:07 -0300

    commitfinal

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { nanoid } from "nanoid";
+import './style.css'
 
 const passwordBtnEl = document.querySelector('button')
 const displayPasswordEL = document.querySelector('h2')
+const copy = import('clipboard-copy')
 
 passwordBtnEl.addEventListener('click', () => {
     const randomPassword = nanoid()
